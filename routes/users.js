b5b7020b70015bbcebb5381df60f05f8deaa7b64
fangzhzh <fangzhzh@gmail.com>
2015-10-30 10:07:49 +0800

    addd users code.

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../model/dbuser');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    User.find({}, function(err, users) {
+        if (err) throw err;
+
+        // object of all the users
+        console.log(users);
+        res.send(users);
+    });
 });
 
+router.post('/', function(req, res, next) {
+    var mike = new User({
+        user_name: req.query.name,
+        password: req.query.password,
+        bind_devices: req.query.bind_device,
+        nick_name:req.query.nick_name
+    });
+    mike.save( function(err){
+        if (err) throw err;
+
+        console.log('user '+mike.user_name + ' saved success');
+    });
+    res.send('respond with from sleep');
+});
+
+router.delete('/:userid',function(req, res, next){
+    User.remove({user_id: req.params.userid}, function(err) {
+        if(error)
+            return next(error);
+        console.log(rreq.params.userid+'delete susscess');
+        res.send(req.params.userid+'delete susscess')
+    })});
+
+
+router.sayHello = function(hello){
+    console.log(hello + " from users module");
+
+} ;
+
+router.getAllUser = function(){
+    User.find({}, function(err, users) {
+        if (err) throw err;
+
+        // object of all the users
+
+        return users;
+    });
+};
+
 module.exports = router;
+
+
+
+
