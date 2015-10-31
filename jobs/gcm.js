1ceb44dda0e80ce3fae4237ae8ac3ec963e65893
Amulya Khare <kharea@garena.com>
2015-10-31 11:04:55 +0800

    added gcm integration

---
+++ jobs/gcm.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhenfangzhang on 30/10/15.
+ */
+var express = require('express');
+var router = express.Router();
+var gcm = require('node-gcm');
+
+// Set up the sender with you API key
+var sender = new gcm.Sender('AIzaSyAEftk9ZuLdOwuyH50le50o8pCkjGVWZeQ');
+
+
+var sendPush = function (user) {
+	var regTokens = [user.pushToken];
+
+	var message = new gcm.Message();
+
+	message.addData('message','Your buddy has nudged you.');
+	message.addNotification('title', 'Nudge');
+	message.addNotification('body', 'Your buddy has nudged you.');
+
+	sender.send(message, { registrationTokens: registrationTokens }, function (err, result) {
+	  if(err) console.error(err);
+	  else    console.log(result);
+	});
+};
+
+router.sendPush = sendPush;
+module.exports = router;
