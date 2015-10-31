8056c832b31386f2cadeced2e0eb1c52db1a9382
Amulya Khare <kharea@garena.com>
2015-10-31 13:32:42 +0800

    prevent login crash

--- auth/passport.js
+++ auth/passport.js
@@ -91,7 +91,7 @@ module.exports = function(passport) {
 
                     // check to see if theres already a user with that email
                     if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                        return done(null, false);
                     } else {
                         
                         // create the user
